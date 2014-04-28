6f80599f240cacb7ce267167b7599071e7169353
Swapnil Sonar <swap.m.sonar@gmail.com>
2014-04-28 18:05:18 +0530

    I have updated the error message.

---
+++ MyFirstGitProject/src/com/vspl/java/git/GitSample.java
@@ -0,0 +1,13 @@
+package com.vspl.java.git;
+
+public class GitSample {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Git is fun");
+	}
+
+}
