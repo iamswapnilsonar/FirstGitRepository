96c34dd3602b2699e2bde8bfe3c381e11e7d3f5d
Swapnil Sonar <swap.m.sonar@gmail.com>
2014-04-30 16:42:05 +0530

    We looking to change the package name of java project. We need not require java name into the package name.

---
+++ MyFirstGitProject/src/com/vspl/git/GitSample.java
@@ -0,0 +1,18 @@
+package com.vspl.git;
+
+public class GitSample {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Git is powerful SCM");
+		
+		Character character = new Character();
+		int temp = character.makeAddition(10, 5);
+		
+		System.out.println("Addition : " + temp);
+	}
+
+}
