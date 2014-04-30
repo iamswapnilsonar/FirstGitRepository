c8db278ecb5901337858f8674d924496aac27e56
Swapnil Sonar <swap.m.sonar@gmail.com>
2014-04-30 11:35:06 +0530

    Use the Character model in example for addition of two numbers.

--- MyFirstGitProject/src/com/vspl/java/git/GitSample.java
+++ MyFirstGitProject/src/com/vspl/java/git/GitSample.java
@@ -8,6 +8,11 @@ public class GitSample {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Git is powerful SCM");
+		
+		Character character = new Character();
+		int temp = character.makeAddition(10, 5);
+		
+		System.out.println("Addition : " + temp);
 	}
 
 }
