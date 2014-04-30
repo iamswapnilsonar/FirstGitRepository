a5b9f234c1186b70859fbea40c49c7558197aab6
Swapnil Sonar <swap.m.sonar@gmail.com>
2014-04-30 11:36:06 +0530

    Introduce the new Character model for addition of two numbers.

---
+++ MyFirstGitProject/src/com/vspl/java/git/Character.java
@@ -0,0 +1,13 @@
+package com.vspl.java.git;
+
+public class Character {
+
+	public Character() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public int makeAddition(int a, int b){
+		return (a+b);
+	}
+	
+}
