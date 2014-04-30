85801d7d13fb7657ba614de651beb30a8d1940f5
Swapnil Sonar <swap.m.sonar@gmail.com>
2014-04-30 17:20:33 +0530

    We adding new framework for showing system messages.

---
+++ MyFirstGitProject/src/com/vspl/git/Logger.java
@@ -0,0 +1,13 @@
+package com.vspl.git;
+
+public class Logger {
+
+	public Logger() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void showLogFile(String message){
+		System.out.println("Logger : "+ message);
+	}
+	
+}
